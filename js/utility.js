5cfaf316e74f680a16f0b9e6a28e1b5817a6185c
thithomazelli <thiago.thomazelli@gmail.com>
2018-12-21 00:27:28 -0200

    Change each index section to individual page.

--- js/utility.js
+++ js/utility.js
@@ -1,32 +1,25 @@
-// $('.nav-link, .navbar-brand, .new-button').click(function() {
-//     var sectionTo = $(this).attr('href');
-//     $('html, body').animate({
-//         scrollTop: $(sectionTo).offset().top + 10
-//     }, 1000);
+// // Cache selectors
+// var lastId,
+//     navMenu = $(".nav-menu"),
+//     navMenuHeight = navMenu.outerHeight()-45,
+//     // All list items
+//     menuItems = navMenu.find("a"),
+//     // Anchors corresponding to menu items
+//     scrollItems = menuItems.map(function(){
+//       var item = $($(this).attr("href"));
+//       if (item.length) { return item; }
+//     });
+
+// // Bind click handler to menu items
+// // so we can get a fancy scroll animation
+// menuItems.click(function(e){
+//   var href = $(this).attr("href"),
+//       offsetTop = href === "#" ? 0 : $(href).offset().top-navMenuHeight+1;
+//   $('html, body').stop().animate({ 
+//       scrollTop: offsetTop
+//   }, 250);
+//   e.preventDefault();
 // });
-
-// Cache selectors
-var lastId,
-    navMenu = $(".nav-menu"),
-    navMenuHeight = navMenu.outerHeight()-45,
-    // All list items
-    menuItems = navMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-navMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 250);
-  e.preventDefault();
-});
 
 function verifyCaptcha() {
     var registerBtn = document.querySelector('#btn-Enviar');
